1a0b461ce1928ac25c7c28c63c423977c0d61fac
devWRM <mcwen@comcast.net>
2021-11-07 10:28:30 -0500

    built GuessList component to list each user code number guess

---
+++ src/components/GuessList.js
@@ -0,0 +1,33 @@
+import React, {  useState } from 'react';
+
+import Guess from './Guess.js';
+
+
+
+
+function GuessList({ codeNumbers, guessList }) {
+    const[list, updateList] = useState("")
+    
+    // debugger;
+
+    let guesses =   guessList.map(guess => {
+                            
+                        return <div key={Math.floor(Math.random() * 10000)}>
+                            {/* <Guess guess={guess} /> */}
+
+                            { guess.join("")}
+                        </div>
+                    })
+
+
+    return (
+        <div>
+                You have { 10 - guessList.length } guesses 
+                
+                { guesses }
+
+        </div>
+    )
+}
+
+export default GuessList;
