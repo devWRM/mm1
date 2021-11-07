ed65389725f817f63783f2dbe20fcfeef67efd75
devWRM <mcwen@comcast.net>
2021-11-07 01:49:17 -0500

    corrected userGuess argument in the handleSubmitUserInput function by removing the quotation marks

--- src/components/Input1.js
+++ src/components/Input1.js
@@ -1,15 +1,45 @@
 import React, { useState } from 'react';
 
-function Input1({ codeNumbers }) {
+function Input1({ codeNumbers, handleUserGuess }) {
+    const[userGuess, updateUserGuess] = useState("")
+
+    function handleUserGuessInput(e) {
+        updateUserGuess(e.target.value)
+    }
+
+    function handleSubmitUserInput(e) {
+// debugger;
+        e.preventDefault();
+
+        if(!!userGuess && (userGuess.length === 4) ) {
+            // handleUserGuess(userGuess)
+            handleUserGuess(userGuess)
+        } else {
+            updateUserGuess("")
+        }
+
+        // Reset state
+        updateUserGuess("")
+    }
+
+
+
+
+
 
 
     return (
-        <form>
+        <form onSubmit={handleSubmitUserInput}>
             
-                <div>
-                    <input type="text"></input>
+                
+                    <label>Enter 4 numbers: 0 - 7</label>
+                    <input  type="text" 
+                            value={userGuess}
+                            onChange={handleUserGuessInput}
+                            style={{width: "150px"}}                            
+                    />
                     <button type="submit">submit</button>
-                </div>
+                
         
         </form>
     )
