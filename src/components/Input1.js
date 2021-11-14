b8714fb54e27d4521c920be08dd3d913c8be6675
devWRM <mcwen@comcast.net>
2021-11-13 19:52:45 -0500

    tested code re: 10 guesses end game

--- src/components/Input1.js
+++ src/components/Input1.js
@@ -128,7 +128,9 @@ function Input1({ handleShowNewGameButton, handleShowInput1, codeNumbers, guessL
         // -- so guessList showing HERE always reflects 1 guess behind
         // -- so has to be triggered earlier due to the delayed update
         // So when 9 guesses are shown in browser, ONLY 8 reflected HERE
-        // So ACTUAL BROWSER 10 guesses is reflected here by (guessList.length > 8) setting guessList to trigger at 9
+        // So ACTUAL BROWSER 10 guesses is reflected here by (guessList.length === 9 -OR- guessList.length > 8) setting guessList to trigger at 9
+        // if(guessList.length = 9) <<= causes an ERROR
+        // if(guessList.length < 9) <<= prematurely ends game after 1st guess
         if(guessList.length > 8) {
             revealSecretCode(true)
             handleShowNewGameButton(true)
